990518027cce2245f2a501e28a07d0ce279ab727
Zaimon <simon14101995@gmail.com>
2026-04-21 15:49:34 +0200

    fini

--- ft_kaboom_1.py
+++ ft_kaboom_1.py
@@ -1,4 +1,4 @@
 print("=== Kaboom 1 ===")
 print("Access to alchemy/grimoire/dark_spellbook.py directly")
 print("Test import now - THIS WILL RAISE AN UNCAUGHT EXCEPTION")
-from alchemy.grimoire.dark_spellbook import dark_spell_record  # noqa: E402, F401
+from alchemy.grimoire.dark_spellbook import dark_spell_record
